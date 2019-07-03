3adc5e1a9d3ddc53b379febc1d56b9d88a3ca8b7
ravireddykiran <36273442+ravireddykiran@users.noreply.github.com>
2019-07-03 10:41:19 +0530

    Create rt.py

---
+++ rt.py
@@ -0,0 +1,11 @@
+num1 = float(input("enter the first nuber":))
+num2 = float(input("enter the second number":))
+num3 = float(input("enter the third number":))
+if (num1 > num2) and (num1 > num3):
+ largest=num1
+if(num2 > num1) and (num2 > num3):
+ largest=num2
+else:
+ largest=num3
+print("the largest number is",largest) 
+  
