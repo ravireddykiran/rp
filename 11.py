9197bd262eac9f2521d697089461da86486c7178
ravireddykiran <36273442+ravireddykiran@users.noreply.github.com>
2019-07-16 21:39:52 -0700

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,10 +1,3 @@
-(basea,p)=map(int,input().split())
-output=1
-#output=pow(basea,p)
-if(z==0):
-  output=1
-else:
-  for i in range(z):
-    output=output*basea
- 
-print(output)
+n,k=map(int,input().split())
+power=pow(n,k)
+print(power)
