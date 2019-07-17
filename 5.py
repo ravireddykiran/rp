3d0fde068566b64803049c6e2a1cc1d26c813ccf
ravireddykiran <36273442+ravireddykiran@users.noreply.github.com>
2019-07-16 22:09:51 -0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,7 +1,7 @@
-land2,ma2,abarna2=map(int,input().split())
+land2,ma2,abarn2=map(int,input().split())
 if(land2==224):
   print("YES")
-elif(land2%(ma2+abarna2)==0):
+elif(land2%(ma2+abarn2)==0):
   print("YES")
 else:
   print("NO")
