a8a215feaa9c4128bb48caacbb6dda3a5cdb97df
ravireddykiran <36273442+ravireddykiran@users.noreply.github.com>
2019-07-16 21:37:36 -0700

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,10 +1,10 @@
 (basea,p)=map(int,input().split())
 output=1
 #output=pow(basea,p)
-if(p==0):
+if(z==0):
   output=1
 else:
-  for i in range(p):
+  for i in range(z):
     output=output*basea
  
 print(output)
